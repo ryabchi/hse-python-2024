c9db1a4f888b7567ec5803eac0c899f858955bd3
Batyr Atayev <atayev2012@gmail.com>
2024-04-12 17:41:23 +0300

    Practice1 completed. All tests passed.

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -9,7 +9,10 @@ def concatenate_strings(a: str, b: str) -> str:
     """
 
     # пиши свой код здесь
-
+    if a.isnumeric() and b.isnumeric():
+        result = str(int(a) + int(b))
+    else:
+        result = a + b
     return result
 
 
@@ -23,5 +26,5 @@ def calculate_salary(total_compensation: int) -> float:
     """
 
     # пиши свой код здесь
-
+    result = total_compensation * 0.87
     return result
