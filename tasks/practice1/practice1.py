db8da4deafad855d78eb187d9a9c48f929a31849
StepanKhrushkov <109822810+StepaKH@users.noreply.github.com>
2024-04-15 21:43:01 +0300

    Update practice1.py

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -9,6 +9,7 @@ def concatenate_strings(a: str, b: str) -> str:
     """
 
     # пиши свой код здесь
+    result = a + b
 
     return result
 
@@ -23,5 +24,6 @@ def calculate_salary(total_compensation: int) -> float:
     """
 
     # пиши свой код здесь
+    result = total_compensation*0.87
 
     return result
