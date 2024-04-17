446a3f59c75a9257a31b8314e0cdb95606278cd6
v.v.nikonov <v.v.nikonov@tinkoff.ru>
2024-04-17 09:28:08 +0300

    Practice 3

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -8,7 +8,7 @@ def concatenate_strings(a: str, b: str) -> str:
     :return: результат сложения
     """
 
-    # пиши свой код здесь
+    result = a + b
 
     return result
 
@@ -21,7 +21,7 @@ def calculate_salary(total_compensation: int) -> float:
     :param total_compensation: сумма зарплаты до вычета налога
     :return: сумма заплаты после вычета налога
     """
-
-    # пиши свой код здесь
+    tax = 0.13
+    result = total_compensation - total_compensation * tax
 
     return result
