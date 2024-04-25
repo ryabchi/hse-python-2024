903101aa94a7aafe723be0dc359a18e7d5dc5f04
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-04-25 08:34:31 +0300

    done practice2

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -12,7 +13,7 @@ def greet_user(name: str) -> str:
     :return: приветствие
     """
 
-    # пиши код здесь
+    greeting = f"Hello, {name}!"
     return greeting
 
 
@@ -28,7 +29,7 @@ def get_amount() -> float:
     :return: случайную сумму на счете
     """
 
-    # пиши код здесь
+    amount = round(random.uniform(100, 1000000), 2)
     return amount
 
 
@@ -42,7 +43,13 @@ def is_phone_correct(phone_number: str) -> bool:
                                           False - если номер некорректный
     """
 
-    # пиши код здесь
+    result = False
+    if phone_number[0] == "+" and len(phone_number) == 12 and phone_number[1] == "7":
+        result = True
+        for i in phone_number[1:]:
+            if not i.isdigit():
+                result = False
+                break
     return result
 
 
@@ -58,7 +65,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
                                           False - если денег недостаточно
     """
 
-    # пиши код здесь
+    result = True if current_amount >= float(transfer_amount) else False
     return result
 
 
@@ -77,7 +84,12 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     :return: текст, соответсвующий правилам
     """
 
-    # пиши код здесь
+    result = text.strip().lower().capitalize()
+    result = result.replace("\'", "").replace("\"", "")
+
+    for word in uncultured_words:
+        result = result.replace(word, len(word) * "#")
+
     return result
 
 
@@ -100,5 +112,6 @@ def create_request_for_loan(user_info: str) -> str:
     :return: текст кредитной заявки
     """
 
-    # пиши код здесь
+    user_data = user_info.split(",")
+    result = f"Фамилия: {user_data[0]}\nИмя: {user_data[1]}\nОтчество: {user_data[2]}\nДата рождения: {user_data[3]}\nЗапрошенная сумма: {user_data[4]}"
     return result
