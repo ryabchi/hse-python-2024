8cc95182d4db8dbda765ee5af88c7b860f05cb2f
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-05-11 21:58:42 +0300

    first three practices completed

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from typing import Dict, Any, List, Optional
+import re
+import csv
 
 
 def count_words(text: str) -> Dict[str, int]:
@@ -26,9 +28,18 @@ def count_words(text: str) -> Dict[str, int]:
              значение - количество вхождений слов в текст
     """
 
-    # пиши свой код здесь
+    words = re.findall(r'\b[a-zA-Z]+\b', text)
+    result = {}
 
-    return {}
+    for word in words:
+        lower_word = word.lower()
+        if lower_word.isalpha():
+            if lower_word in result:
+                result[lower_word] += 1
+            else:
+                result[lower_word] = 1
+
+    return result
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
@@ -42,7 +53,7 @@ def exp_list(numbers: List[int], exp: int) -> List[int]:
 
     # пиши свой код здесь
 
-    return []
+    return [number ** exp for number in numbers]
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
@@ -57,6 +68,13 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     :param special_category: список категорий повышенного кешбека
     :return: размер кешбека
     """
+
+    result = 0.0
+    for operation in operations:
+        if operation['category'] in special_category:
+            result += operation['amount'] * 0.05
+        else:
+            result += operation['amount'] * 0.01
 
     return result
 
@@ -99,6 +117,11 @@ def csv_reader(header: str) -> int:
     :return: количество уникальных элементов в столбце
     """
 
-    # пиши свой код здесь
+    unique_values = set()
 
-    return 0
+    with open(get_path_to_file(), 'r', newline='') as file:
+        data = csv.DictReader(file)
+        for row in data:
+            unique_values.add(row[header])
+
+    return len(unique_values)
