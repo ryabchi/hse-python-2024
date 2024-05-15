6f90e5470c7a580d37176be1a24257f1de78251e
sadness52r <bukin.k2017@gmail.com>
2024-05-15 14:51:09 +0300

    tasks done

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -13,6 +14,7 @@ def greet_user(name: str) -> str:
     """
 
     # пиши код здесь
+    greeting = f"Hello, {name}!"
     return greeting
 
 
@@ -29,6 +31,7 @@ def get_amount() -> float:
     """
 
     # пиши код здесь
+    amount = round(random.uniform(100, 1000001), 2)
     return amount
 
 
@@ -43,6 +46,7 @@ def is_phone_correct(phone_number: str) -> bool:
     """
 
     # пиши код здесь
+    result = phone_number[0:2] == '+7' and phone_number[2:].isnumeric() and len(phone_number) == 12
     return result
 
 
@@ -59,6 +63,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
+    result = current_amount >= float(transfer_amount)
     return result
 
 
@@ -78,6 +83,10 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
 
     # пиши код здесь
+    text = text.strip().replace("'", '').replace('"', '')
+    for unc_word in uncultured_words:
+        text = text.replace(unc_word, '#' * len(unc_word))
+    result = " ".join(text.capitalize().split())
     return result
 
 
@@ -101,4 +110,6 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
+    user_info_lst = user_info.split(',')
+    result = f"Фамилия: {user_info_lst[0]}\nИмя: {user_info_lst[1]}\nОтчество: {user_info_lst[2]}\nДата рождения: {user_info_lst[3]}\nЗапрошенная сумма: {user_info_lst[4]}"
     return result
