70599d925712ef60ca6d83e750dc8a01738e5956
codlxflare <148866504+codlxflare@users.noreply.github.com>
2024-05-26 15:27:32 +0300

    Complite tests

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,104 +1,56 @@
 from pathlib import Path
 from typing import Dict, Any, List, Optional
 
+import re
+from collections import Counter
 
 def count_words(text: str) -> Dict[str, int]:
-    """
-    Функция для подсчета слов в тексте.
+    words = re.findall(r'\b[a-zA-Z]{2,}\b', text)
 
-    При подсчете слов - все знаки препинания игнорируются.
-    Словом считается непрерывная последовательность длиной больше одного
-    символа, состоящая из букв в диапазоне A-Z и a-z.
-    Если в последовательности присутствует цифра - это не слово.
+    word_count = {}
 
-    Hello - слово
-    Hello7 - не слово
+    for word in words:
+        word_lower = word.lower()
+        if word_lower in word_count:
+            word_count[word_lower] += 1
+        else:
+            word_count[word_lower] = 1
 
-    При подсчете слов регистр букв не имеет значения.
+    return word_count
 
-    Результат выполнения функции словарь, в котором:
-    ключ - слово в нижнем регистре
-    значение - количество вхождений слов в текст
-
-    :param text: текст, для подсчета символов
-    :return: словарь, в котором:
-             ключ - слово в нижнем регистре
-             значение - количество вхождений слов в текст
-    """
-
-    # пиши свой код здесь
-
-    return {}
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
-    """
-    Функция, которая возводит каждый элемент списка в заданную степень
+    return [num ** exp for num in numbers]
 
-    :param numbers: список, состоящий из натуральных чисел
-    :param exp: в какую степень возвести числа в списке
-    :return: список натуральных чисел
-    """
-
-    # пиши свой код здесь
-
-    return []
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
-    """
-    Функция для расчета кешбека по операциям.
-    За покупки в обычных категориях возвращается 1% от стоимости покупки
-    За покупки в special_category начисляют 5% от стоимости покупки
+    cashback = 0
+    for operation in operations:
+        if operation['category'] in special_category:
+            cashback += operation['amount'] * 0.05
+        else:
+            cashback += operation['amount'] * 0.01
+    return cashback
 
-    :param operations: список словарей, содержащих поля
-           amount - сумма операции
-           category - категория покупки
-    :param special_category: список категорий повышенного кешбека
-    :return: размер кешбека
-    """
-
-    return result
 
 
 def get_path_to_file() -> Optional[Path]:
-    """
-    Находит корректный путь до тестового файла.
 
-    Если запускать тесты из pycharm - начальная папка - tests
-    Если запускать файлы через make tests - начальная папка - корень проекта
-
-    :return: путь до тестового файла tasks.csv
-    """
     if Path().resolve().name == 'tests':
         base_path = Path().resolve().parent
     else:
         base_path = Path().resolve()
     return base_path / 'tasks' / 'practice3' / 'tasks.csv'
 
+import csv
 
 def csv_reader(header: str) -> int:
-    """
-    Функция считывает csv файл и подсчитывает количество
-    уникальных элементов в столбце.
-    Столбец выбирается на основе имени заголовка,
-    переданного в переменной header.
+    with open(get_path_to_file(), 'r') as f:
+        reader = csv.DictReader(f)
+        unique_elements = set()
+        for row in reader:
+            unique_elements.add(row[header])
+        return len(unique_elements)
 
-    Обратите внимание на структуру файла!
-    Первая строка - строка с заголовками.
-    Остальные строки - строки с данными.
-
-    Файл для анализа: tasks.csv
-    Для того чтобы файл корректно открывался в тестах:
-    для получения пути до файла - используйте функцию get_path_to_file
-    которая определена перед функцией.
-
-    CSV анализируем с помощью встроенной библиотеки csv
-
-    :param header: название заголовка
-    :return: количество уникальных элементов в столбце
-    """
-
-    # пиши свой код здесь
-
-    return 0
--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -2,66 +2,33 @@ from typing import Set
 from .employee import Employee, Manager
 from .exception import NoSuchMemberError
 
-
 class Team:
-    """
-    Класс - команда.
-    У каждой команды есть менеджер, название и участники.
-
-    Возможности:
-    - добавление участников
-    - удаление участника из команды
-    - просмотр базовой информации об участниках
-    - получение списка участников
-    """
-
     name: str
     manager: Manager
     __members: Set[Employee]
 
     def __init__(self, name: str, manager: Manager):
-        """
-        Задача:
-        Реализовать конструктор класса.
-        Конструктор должен присвоить значения публичным атрибутам
-        и инициализировать контейнер `__members`
-        """
-
-        # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
-        """
-        Задача: реализовать метод добавления участника в команду.
-        Добавить можно только работника.
-        """
-
-        # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError()
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
-        """
-        Задача: реализовать метод удаления участника из команды.
-        Если в команде нет такого участника поднимается исключение `NoSuchMemberError`
-        """
-
-        # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError()
+        if member not in self.__members:
+            raise NoSuchMemberError(self.name, member)
+        self.__members.remove(member)
 
     def get_members(self) -> Set[Employee]:
-        """
-        Задача: реализовать метод возвращения списка участков команды та,
-        чтобы из вне нельзя было поменять список участников внутри класса
-        """
-
-        # пиши свой код здесь
+        return set(self.__members)
 
     def show(self) -> None:
-        """
-        DO NOT EDIT!
-        Данный метод нельзя редактировать!
-
-        Метод показывает информацию о команде в формате:
-        `'team: {team_name} manager: {manager_name} number of members: {members_count)}'`
-
-        Задача: доработать класс таким образом, чтобы метод выполнял свою функцию, не меняя содержимое
-        этого метода
-        """
         print(self)
+
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
