ab73a6d383e1a1fd9b02af3fb01ecf0c1a7af04c
alexshevvv <alekseyshev5@gmail.com>
2024-05-27 20:38:30 +0300

    practice 4 & 5

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -37,15 +37,19 @@ class Employee:
         """
         Задача: реализовать конструктор класса, чтобы все тесты проходили
         """
+        if not isinstance(salary, int) or salary <= 0:
+            raise ValueError("Salary must be a positive integer")
 
-        # пиши свой код здесь
+        self.name = name
+        self.position = position
+        self._salary = salary
 
     def get_salary(self) -> int:
         """
         Метод возвращает зарплату сотрудника.
         """
 
-        # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -54,8 +58,13 @@ class Employee:
         Сравнение происходит по уровню позиции см. `get_position_level`.
         Если что-то идет не так - бросаются исключения. Смотрим что происходит в тестах.
         """
+        if not isinstance(other, Employee):
+            raise TypeError("Comparison with non-Employee object is not supported")
 
-        # пиши свой код здесь
+        try:
+            return get_position_level(self.position) == get_position_level(other.position)
+        except NoSuchPositionError:
+            raise ValueError("One of the employees has an invalid position")
 
     def __str__(self):
         """
@@ -63,7 +72,7 @@ class Employee:
         Пример вывода: 'name: Ivan position manager'
         """
 
-        # пиши свой код здесь
+        return f'name: {self.name} position: {self.position}'
 
     def __hash__(self):
         return id(self)
@@ -82,7 +91,8 @@ class Developer(Employee):
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
 
-        # пиши свой код здесь
+        super().__init__(name, self.position, salary)
+        self.language = language
 
 
 class Manager(Employee):
@@ -97,4 +107,4 @@ class Manager(Employee):
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
 
-        # пиши свой код здесь
+        super().__init__(name, self.position, salary)
--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -27,31 +27,43 @@ class Team:
         и инициализировать контейнер `__members`
         """
 
-        # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
         """
         Задача: реализовать метод добавления участника в команду.
         Добавить можно только работника.
         """
+        if not isinstance(member, Employee):
+            raise TypeError("Member must be an instance of Employee")
 
-        # пиши свой код здесь
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
         """
         Задача: реализовать метод удаления участника из команды.
         Если в команде нет такого участника поднимается исключение `NoSuchMemberError`
         """
-
-        # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError("Member must be an instance of Employee")
+        if member not in self.__members:
+            raise NoSuchMemberError(self.name, member)
+        self.__members.remove(member)
 
     def get_members(self) -> Set[Employee]:
         """
         Задача: реализовать метод возвращения списка участков команды та,
         чтобы из вне нельзя было поменять список участников внутри класса
         """
+        return self.__members.copy()
 
-        # пиши свой код здесь
+    def __str__(self):
+        """
+        Реализовать строковое представление объекта.
+        """
+        return f"team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}"
 
     def show(self) -> None:
         """
