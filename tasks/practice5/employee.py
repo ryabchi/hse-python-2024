18e2e8042e4aa9039b03cb34fae27f474914ba85
StepanKhrushkov <109822810+StepaKH@users.noreply.github.com>
2024-06-04 07:41:28 +0300

    Update employee.py

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -39,6 +39,11 @@ class Employee:
         """
 
         # пиши свой код здесь
+        if not isinstance(salary, int):
+            raise ValueError
+        self.name = name
+        self.position = position
+        self._salary = salary
 
     def get_salary(self) -> int:
         """
@@ -46,6 +51,7 @@ class Employee:
         """
 
         # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -56,6 +62,12 @@ class Employee:
         """
 
         # пиши свой код здесь
+        if not isinstance(other, Employee):
+            raise TypeError()
+        try:
+            return get_position_level(self.position) == get_position_level(other.position)
+        except NoSuchPositionError:
+            raise ValueError()
 
     def __str__(self):
         """
@@ -64,6 +76,7 @@ class Employee:
         """
 
         # пиши свой код здесь
+        return f'name: {self.name} position: {self.position}'
 
     def __hash__(self):
         return id(self)
@@ -83,6 +96,8 @@ class Developer(Employee):
         """
 
         # пиши свой код здесь
+        super().__init__(name, Developer.position, salary)
+        self.language = language
 
 
 class Manager(Employee):
@@ -98,3 +113,4 @@ class Manager(Employee):
         """
 
         # пиши свой код здесь
+        super().__init__(name, Manager.position, salary)
