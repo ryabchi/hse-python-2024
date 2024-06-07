45b8bb8a6e8f76653693508adb5392b7696fdb5a
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-06-07 08:57:34 +0300

    Update team.py

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -27,7 +27,9 @@ class Team:
         и инициализировать контейнер `__members`
         """
 
-        # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
         """
@@ -35,7 +37,10 @@ class Team:
         Добавить можно только работника.
         """
 
-        # пиши свой код здесь
+        if isinstance(member, Employee):
+            self.__members.add(member)
+        else:
+            raise TypeError
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -43,7 +48,12 @@ class Team:
         Если в команде нет такого участника поднимается исключение `NoSuchMemberError`
         """
 
-        # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError
+        try:
+            self.__members.remove(member)
+        except:
+            raise NoSuchMemberError
 
     def get_members(self) -> Set[Employee]:
         """
@@ -51,7 +61,8 @@ class Team:
         чтобы из вне нельзя было поменять список участников внутри класса
         """
 
-        # пиши свой код здесь
+        members = self.__members.copy()
+        return members
 
     def show(self) -> None:
         """
@@ -65,3 +76,6 @@ class Team:
         этого метода
         """
         print(self)
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
+
