240dec1ae4082ee5ce5257e59f821787dc5023c3
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2024-06-07 23:57:48 +0300

    practice5 is done

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -29,6 +29,13 @@ class Team:
 
         # пиши свой код здесь
 
+        self.name = name
+        self.manager = manager
+        self.__members = set()
+
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
+
     def add_member(self, member: Employee) -> None:
         """
         Задача: реализовать метод добавления участника в команду.
@@ -36,6 +43,10 @@ class Team:
         """
 
         # пиши свой код здесь
+
+        if not isinstance(member, Employee):
+            raise TypeError
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -45,6 +56,13 @@ class Team:
 
         # пиши свой код здесь
 
+        if not isinstance(member, Employee):
+            raise TypeError
+        if member not in self.__members:
+            raise NoSuchMemberError(self.name, member)
+        self.__members.remove(member)
+
+
     def get_members(self) -> Set[Employee]:
         """
         Задача: реализовать метод возвращения списка участков команды та,
@@ -52,6 +70,8 @@ class Team:
         """
 
         # пиши свой код здесь
+
+        return self.__members.copy()
 
     def show(self) -> None:
         """
