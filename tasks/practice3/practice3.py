5b0e73b6cea4a3c19528c3d67439c49309dcc866
andre <andreyzepozitive@yandex.ru>
2024-06-12 01:58:28 +0300

    practice3-completed

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,5 +1,8 @@
+from csv import reader as csvrd
 from pathlib import Path
 from typing import Dict, Any, List, Optional
+from string import ascii_lowercase as letters
+from string import punctuation as punct
 
 
 def count_words(text: str) -> Dict[str, int]:
@@ -27,8 +30,26 @@ def count_words(text: str) -> Dict[str, int]:
     """
 
     # пиши свой код здесь
-
-    return {}
+    d = {}
+    for symbol in punct:
+        text = text.replace(symbol, '')
+    data = text.lower().split()
+    DIGIT_IN_WORD = False
+    word_lenght = 0
+    for word in data:
+        DIGIT_IN_WORD = False
+        word_lenght = 0
+        if word not in d:
+            for symbol in word:
+                word_lenght += 1
+                if symbol not in letters:
+                    DIGIT_IN_WORD = True
+                    break
+            if not DIGIT_IN_WORD and word_lenght >= 2:
+                d[word] = 1
+        else:
+            d[word] += 1
+    return d
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
@@ -41,8 +62,8 @@ def exp_list(numbers: List[int], exp: int) -> List[int]:
     """
 
     # пиши свой код здесь
-
-    return []
+    result = [x ** exp for x in numbers]
+    return result
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
@@ -57,7 +78,12 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     :param special_category: список категорий повышенного кешбека
     :return: размер кешбека
     """
-
+    result = 0
+    for operation in operations:
+        if operation['category'] in special_category:
+            result += operation['amount'] * 0.05
+        else:
+            result += operation['amount'] * 0.01
     return result
 
 
@@ -98,7 +124,16 @@ def csv_reader(header: str) -> int:
     :param header: название заголовка
     :return: количество уникальных элементов в столбце
     """
-
     # пиши свой код здесь
-
-    return 0
+    k = 0
+    answer = []
+    with open(get_path_to_file()) as tasks:
+        csv_readed = csvrd(tasks)
+        for index, raw in enumerate(csv_readed):
+            if index == 0:
+                k = raw.index(header)
+            else:
+                raw[k] = raw[k].replace('\n', '')
+                if raw[k] not in answer:
+                    answer.append(raw[k])
+    return len(answer)
