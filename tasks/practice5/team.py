86ff1b0d71bc950f82a588049b7d700d8654cc99
Bbeelina <miss.belay@inbox.ru>
2024-06-12 22:38:44 +0300

    12.06

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -28,6 +28,9 @@ class Team:
         """
 
         # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
         """
@@ -36,6 +39,10 @@ class Team:
         """
 
         # пиши свой код здесь
+        if isinstance(member, Employee):
+            self.__members.add(member)
+        else:
+            raise TypeError("Only employees can be added to the team")
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -44,6 +51,12 @@ class Team:
         """
 
         # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError("member is not Employee")
+        elif member not in self.__members:
+            raise NoSuchMemberError(self.name, member)
+        else:
+            self.__members.remove(member)
 
     def get_members(self) -> Set[Employee]:
         """
@@ -52,6 +65,10 @@ class Team:
         """
 
         # пиши свой код здесь
+        return set(self.__members)
+
+    def __str__(self):
+        return f"team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}"
 
     def show(self) -> None:
         """
