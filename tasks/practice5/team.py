c686f140284a60bff56f3136b4ca01573db078cc
alexei <def29229@icloud.com>
2024-06-16 15:46:59 +0300

    practice 5

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -27,7 +27,9 @@ class Team:
         и инициализировать контейнер `__members`
         """
 
-        # пиши свой код здесь
+        self.name=name
+        self.manager=manager
+        self.__members=set()
 
     def add_member(self, member: Employee) -> None:
         """
@@ -35,7 +37,10 @@ class Team:
         Добавить можно только работника.
         """
 
-        # пиши свой код здесь
+        if isinstance(member, Employee):
+            self.__members.add(member)
+        else:
+            raise TypeError
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -43,7 +48,14 @@ class Team:
         Если в команде нет такого участника поднимается исключение `NoSuchMemberError`
         """
 
-        # пиши свой код здесь
+        if (member in self.__members)and(isinstance(member,Employee)):
+            self.__members.remove(member)
+        elif(not isinstance(member,Employee)):
+            raise TypeError
+        elif(member not in self.__members):
+            raise NoSuchMemberError(self.name, member)
+
+
 
     def get_members(self) -> Set[Employee]:
         """
@@ -51,7 +63,7 @@ class Team:
         чтобы из вне нельзя было поменять список участников внутри класса
         """
 
-        # пиши свой код здесь
+        return set(self.__members)
 
     def show(self) -> None:
         """
@@ -65,3 +77,6 @@ class Team:
         этого метода
         """
         print(self)
+
+    def __str__(self):
+        return f"team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}"
