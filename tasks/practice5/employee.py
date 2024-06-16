dba33ce391eb4e1c653d79d70f7fe7b4fa5aaa77
gurusooo <130143477+gurusooo@users.noreply.github.com>
2024-06-16 10:12:53 +0300

    Update employee.py

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -38,14 +38,17 @@ class Employee:
         Задача: реализовать конструктор класса, чтобы все тесты проходили
         """
 
-        # пиши свой код здесь
+        self.name = name
+        self.position = position
+        self._salary = salary
+
 
     def get_salary(self) -> int:
         """
         Метод возвращает зарплату сотрудника.
         """
 
-        # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -55,7 +58,7 @@ class Employee:
         Если что-то идет не так - бросаются исключения. Смотрим что происходит в тестах.
         """
 
-        # пиши свой код здесь
+        return get_position_level(self.position) == get_position_level(other.position)
 
     def __str__(self):
         """
@@ -63,7 +66,7 @@ class Employee:
         Пример вывода: 'name: Ivan position manager'
         """
 
-        # пиши свой код здесь
+        return f"name: {self.name} position: {self.position}"
 
     def __hash__(self):
         return id(self)
@@ -82,7 +85,8 @@ class Developer(Employee):
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
 
-        # пиши свой код здесь
+        super().__init__(name, 'developer', salary)
+        self.language = language
 
 
 class Manager(Employee):
@@ -97,4 +101,4 @@ class Manager(Employee):
         Задача: реализовать конструктор класса, используя конструктор родителя
         """
 
-        # пиши свой код здесь
+        super().__init__(name, 'manager', salary)
