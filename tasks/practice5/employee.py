bb1333de26df3ccf16a9446297fe83e5b7b0316d
tombombon <timkasuperkeks@gmail.com>
2024-06-16 13:25:38 +0300

    all practice (сначала только первая хотела выгружаться, непонятно, почему)

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -1,6 +1,5 @@
 from typing import Dict
-
-from .exception import NoSuchPositionError
+from tasks.practice5.exception import NoSuchPositionError, EmployeeError
 
 POSITIONS: Dict[str, int] = {
     'CEO': 0,
@@ -40,12 +39,20 @@ class Employee:
 
         # пиши свой код здесь
 
+        if not isinstance(salary, int):
+            raise ValueError("Зарплата должна быть целым числом")
+        self.name = name
+        self.position = position
+        self._salary = salary
+
     def get_salary(self) -> int:
         """
         Метод возвращает зарплату сотрудника.
         """
 
         # пиши свой код здесь
+
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -57,6 +64,12 @@ class Employee:
 
         # пиши свой код здесь
 
+        if not isinstance(other, Employee):
+            raise TypeError("Сравнение может быть только между работниками")
+        try:
+            return get_position_level(self.position) == get_position_level(other.position)
+        except NoSuchPositionError as e:
+            raise ValueError(f"Ошибка сравнения позиции: {e}")
     def __str__(self):
         """
         Задача: реализовать строковое представление объекта.
@@ -64,6 +77,8 @@ class Employee:
         """
 
         # пиши свой код здесь
+
+        return f'name: {self.name} position: {self.position}'
 
     def __hash__(self):
         return id(self)
@@ -84,6 +99,9 @@ class Developer(Employee):
 
         # пиши свой код здесь
 
+        super().__init__(name, self.position, salary)
+        self.language = language
+
 
 class Manager(Employee):
     """
@@ -98,3 +116,5 @@ class Manager(Employee):
         """
 
         # пиши свой код здесь
+
+        super().__init__(name, self.position, salary)
--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -1,6 +1,6 @@
 from typing import Set
-from .employee import Employee, Manager
-from .exception import NoSuchMemberError
+from tasks.practice5.employee import Employee, Manager
+from tasks.practice5.exception import NoSuchMemberError
 
 
 class Team:
@@ -29,6 +29,10 @@ class Team:
 
         # пиши свой код здесь
 
+        self.name = name
+        self.manager = manager
+        self.__members = set()
+
     def add_member(self, member: Employee) -> None:
         """
         Задача: реализовать метод добавления участника в команду.
@@ -36,6 +40,10 @@ class Team:
         """
 
         # пиши свой код здесь
+
+        if not isinstance(member, Employee):
+            raise TypeError("Участник должен быть работником")
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -45,6 +53,12 @@ class Team:
 
         # пиши свой код здесь
 
+        if not isinstance(member, Employee):
+            raise TypeError("Участник должен быть работником")
+        if member not in self.__members:
+            raise NoSuchMemberError(self.name, member)
+        self.__members.remove(member)
+
     def get_members(self) -> Set[Employee]:
         """
         Задача: реализовать метод возвращения списка участков команды та,
@@ -52,6 +66,8 @@ class Team:
         """
 
         # пиши свой код здесь
+
+        return self.__members.copy()
 
     def show(self) -> None:
         """
@@ -65,3 +81,6 @@ class Team:
         этого метода
         """
         print(self)
+
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
