9c1dbe62e6a340a20d5b8864cfeb3004a7bdd390
Bat0nNcH1k <idshalyavin@gmail.com>
2024-06-17 21:38:01 +0300

    Never gonna give you up
    Never gonna give you up, never gonna let you down
    Never gonna run around and desert you
    Never gonna make you cry, never gonna say goodbye
    Never gonna tell a lie and hurt you

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -39,6 +39,12 @@ class Employee:
         """
 
         # пиши свой код здесь
+        if type(salary) is int:
+            self.name = name
+            self.position = position
+            self._salary = salary
+        else:
+            raise ValueError
 
     def get_salary(self) -> int:
         """
@@ -46,6 +52,7 @@ class Employee:
         """
 
         # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -56,6 +63,12 @@ class Employee:
         """
 
         # пиши свой код здесь
+        if not isinstance(other, Employee):
+            raise TypeError
+        try:
+            return get_position_level(self.position) == get_position_level(other.position)
+        except:
+            raise ValueError
 
     def __str__(self):
         """
@@ -64,6 +77,7 @@ class Employee:
         """
 
         # пиши свой код здесь
+        return f"name: {self.name} position: {self.position}"
 
     def __hash__(self):
         return id(self)
@@ -83,7 +97,8 @@ class Developer(Employee):
         """
 
         # пиши свой код здесь
-
+        super().__init__(name, self.position, salary)
+        self.language = language
 
 class Manager(Employee):
     """
@@ -98,3 +113,4 @@ class Manager(Employee):
         """
 
         # пиши свой код здесь
+        super().__init__(name, self.position, salary)
