197f2209066c309b04564daaaeefe5a306eed734
Lexenburgir <apodoiko42@gmail.com>
2024-06-17 23:03:12 +0300

    qas

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -10,7 +10,9 @@ def concatenate_strings(a: str, b: str) -> str:
 
     # пиши свой код здесь
 
-    return a + b
+    result = a + b
+
+    return result
 
 
 def calculate_salary(total_compensation: int) -> float:
@@ -24,4 +26,6 @@ def calculate_salary(total_compensation: int) -> float:
 
     # пиши свой код здесь
 
-    return total_compensation * 0.87
+    result = total_compensation * 0.87
+
+    return result
