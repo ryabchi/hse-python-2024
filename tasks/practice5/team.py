e084e2fd6bb1c621e7e765d47df45983f3558bd6
konovdaniil926@gmail.com <konovdaniil926@gmail.com>
2024-06-17 23:46:10 +0300

    Рейкьявик

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -28,6 +28,9 @@ class Team:
         """
 
         # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
         """
@@ -36,6 +39,10 @@ class Team:
         """
 
         # пиши свой код здесь
+        if isinstance(member, Employee):
+            self.__members.add(member)
+        else:
+            raise TypeError
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -45,6 +52,14 @@ class Team:
 
         # пиши свой код здесь
 
+        if not isinstance(member, Employee):
+            raise TypeError
+        
+        if member in self.__members:
+            self.__members.remove(member)
+        else:
+            raise NoSuchMemberError(self.name, member)
+
     def get_members(self) -> Set[Employee]:
         """
         Задача: реализовать метод возвращения списка участков команды та,
@@ -52,6 +67,8 @@ class Team:
         """
 
         # пиши свой код здесь
+        
+        return set(self.__members)
 
     def show(self) -> None:
         """
@@ -65,3 +82,6 @@ class Team:
         этого метода
         """
         print(self)
+
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
