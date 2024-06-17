e7ccf74b3cb3c40dd749629ba9b632ecea3e4eb9
konovdaniil926@gmail.com <konovdaniil926@gmail.com>
2024-06-17 19:53:13 +0300

    Блин, я второй

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -13,6 +14,9 @@ def greet_user(name: str) -> str:
     """
 
     # пиши код здесь
+
+    greeting = f"Божиею поспешествующею милостию {name}, император и самодержец Всероссийский, Московский, Киевский, Владимирский, Новгородский; царь Казанский, царь Астраханский, царь Польский, царь Сибирский, царь Херсонеса Таврического, царь Грузинский; государь Псковский и великий князь Смоленский, Литовский, Волынский, Подольский и Финляндский; князь Эстляндский, Лифляндский, Курляндский и Семигальский, Самогитский, Белостокский, Корельский, Тверский, Югорский, Пермский, Вятский, Болгарский и иных; государь и великий князь Новагорода низовския земли, Черниговский, Рязанский, Полотский, Ростовский, Ярославский, Белозерский, Удорский, Обдорский, Кондийский, Витебский, Мстиславский и всея северныя страны повелитель; и государь Иверския, Карталинския и Кабардинския земли и области Арменския; Черкасских и Горских князей и иных наследный государь и обладатель, государь Туркестанский; наследник Норвежский, герцог Шлезвиг-Голштейнский, Стормарнский, Дитмарсенский и Ольденбургский и прочая, и прочая, и прочая"
+
     return greeting
 
 
@@ -29,6 +33,10 @@ def get_amount() -> float:
     """
 
     # пиши код здесь
+
+    amount = random.randint(100 * 100, 1000000 * 100)
+    amount /= 100
+
     return amount
 
 
@@ -43,6 +51,12 @@ def is_phone_correct(phone_number: str) -> bool:
     """
 
     # пиши код здесь
+    
+    result = True
+    result &= len(phone_number) == 12
+    result &= phone_number[:2] == "+7"
+    result &= phone_number[1:].isnumeric()
+
     return result
 
 
@@ -59,6 +73,9 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
+
+    result = current_amount >= float(transfer_amount)
+
     return result
 
 
@@ -78,6 +95,18 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
 
     # пиши код здесь
+
+    text = text.lower()
+    uncultured_words = [plohoe_slovo.lower() for plohoe_slovo in uncultured_words]
+    text = text.translate({ord(character): None for character in '\'"'})
+    smeshnaya_narezka = text.split(' ')
+    smeshnaya_narezka = [slovo for slovo in smeshnaya_narezka if slovo != '']
+    result = ' '.join(smeshnaya_narezka)
+    for plohoe_slovo in uncultured_words:
+        while plohoe_slovo in result:
+            result = result.replace(plohoe_slovo, len(plohoe_slovo) * '#')
+    result = result.capitalize()
+    
     return result
 
 
@@ -101,4 +130,12 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
+
+    porezal = user_info.split(",")
+    result = "Фамилия: "             + porezal[0] +\
+             "\nИмя: "               + porezal[1] +\
+             "\nОтчество: "          + porezal[2] +\
+             "\nДата рождения: "     + porezal[3] +\
+             "\nЗапрошенная сумма: " + porezal[4]
+
     return result
