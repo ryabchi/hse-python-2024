54fd341b57190f306742a75101e61d284116ec9a
Midas-XLR <93703127+Ramzzz10@users.noreply.github.com>
2024-06-22 20:34:46 +0300

    Add files via upload

---
+++ tasks/practice3/practice3.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+from typing import Dict, Any, List, Optional
+
+import re
+
+
+def count_words(text: str) -> Dict[str, int]:
+    """
+    Функция для подсчета слов в тексте.
+
+    При подсчете слов - все знаки препинания игнорируются.
+    Словом считается непрерывная последовательность длиной больше одного
+    символа, состоящая из букв в диапазоне A-Z и a-z.
+    Если в последовательности присутствует цифра - это не слово.
+
+    Hello - слово
+    Hello7 - не слово
+
+    При подсчете слов регистр букв не имеет значения.
+
+    Результат выполнения функции словарь, в котором:
+    ключ - слово в нижнем регистре
+    значение - количество вхождений слов в текст
+
+    :param text: текст, для подсчета символов
+    :return: словарь, в котором:
+             ключ - слово в нижнем регистре
+             значение - количество вхождений слов в текст
+    """
+
+    # пиши свой код здесь
+    pattern = r'\b[a-zA-Z]+\b'
+    counter = {}
+    for word in re.findall(pattern, text):
+        word = word.lower()
+        counter[word] = counter.get(word, 0) + 1
+    return counter
+
+
+def exp_list(numbers: List[int], exp: int) -> List[int]:
+    """
+    Функция, которая возводит каждый элемент списка в заданную степень
+
+    :param numbers: список, состоящий из натуральных чисел
+    :param exp: в какую степень возвести числа в списке
+    :return: список натуральных чисел
+    """
+
+    # пиши свой код здесь
+    result = list(map(lambda x: x ** exp, numbers))
+    return result
+
+
+def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
+    """
+    Функция для расчета кешбека по операциям.
+    За покупки в обычных категориях возвращается 1% от стоимости покупки
+    За покупки в special_category начисляют 5% от стоимости покупки
+
+    :param operations: список словарей, содержащих поля
+           amount - сумма операции
+           category - категория покупки
+    :param special_category: список категорий повышенного кешбека
+    :return: размер кешбека
+    """
+    result = 0
+    for operation in operations:
+        k = 0.05 if operation['category'] in special_category else 0.01
+        result += operation['amount'] * k
+    return result
+
+
+def get_path_to_file() -> Optional[Path]:
+    """
+    Находит корректный путь до тестового файла.
+
+    Если запускать тесты из pycharm - начальная папка - tests
+    Если запускать файлы через make tests - начальная папка - корень проекта
+
+    :return: путь до тестового файла tasks.csv
+    """
+    if Path().resolve().name == 'tests':
+        base_path = Path().resolve().parent
+    else:
+        base_path = Path().resolve()
+    return base_path / 'tasks' / 'practice3' / 'tasks.csv'
+
+
+def csv_reader(header: str) -> int:
+    """
+    Функция считывает csv файл и подсчитывает количество
+    уникальных элементов в столбце.
+    Столбец выбирается на основе имени заголовка,
+    переданного в переменной header.
+
+    Обратите внимание на структуру файла!
+    Первая строка - строка с заголовками.
+    Остальные строки - строки с данными.
+
+    Файл для анализа: tasks.csv
+    Для того чтобы файл корректно открывался в тестах:
+    для получения пути до файла - используйте функцию get_path_to_file
+    которая определена перед функцией.
+
+    CSV анализируем с помощью встроенной библиотеки csv
+
+    :param header: название заголовка
+    :return: количество уникальных элементов в столбце
+    """
+
+    # пиши свой код здесь
+    # ^ А лучше на расте:
+    # P.S. Главный Фанат Раста
+    def remove_brackets(word: str) -> str:
+        if word.startswith('"') and word.endswith('"') or word.startswith("'") and word.endswith("'"):
+            word = word[1:-1]
+        return word
+
+    def read_csv_row(row: str):
+        return list(map(remove_brackets, row.strip().split(',')))
+
+    counter = {}
+    with open(get_path_to_file(), 'r') as file:
+        columns      = read_csv_row(file.readline())
+        column_index = columns.index(header)
+        for data in file.readlines():
+            value          = read_csv_row(data)[column_index]
+            counter[value] = counter.get(value, 0) + 1
+    return len(counter)
